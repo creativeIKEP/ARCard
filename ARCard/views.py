e47c55b6ef914c8ed52789672aed01119885d15c
IKEP <34697515+creativeIKEP@users.noreply.github.com>
2020-09-07 21:24:44 +0900

    add template and static files

--- ARCard/views.py
+++ ARCard/views.py
@@ -2,5 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world. ar card index")
-    #return render(request, 'index.html', {})
+    return render(request, 'index.html', {})
---
+++ static/hoge.js
@@ -0,0 +1,3 @@
+const hoge = () => {
+  alert("hello")
+}
